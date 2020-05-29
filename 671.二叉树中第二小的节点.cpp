24d1bf3e45ab976503777b9081e21f45c593b643
Hyong <hy4165yh@gmail.com>
2020-05-29 22:30:11 +0800

    完成

--- 671.二叉树中第二小的节点.cpp
+++ 671.二叉树中第二小的节点.cpp
@@ -14,32 +14,40 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
  */
-#include <iostream>
-#include <set>
-using namespace std;
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+// #include <iostream>
+// #include <set>
+// using namespace std;
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// };
 class Solution {
 public:
-    int ans = INT32_MAX;
+    long int ans = INT64_MAX;
     set<int> temp;
     int findSecondMinimumValue(TreeNode* root) {
-        if(root == nullptr || (root->left == nullptr && root->right == nullptr))
-            return ans;
-        temp.insert(root->val);
-        temp.insert(root->left->val);
-        temp.insert(root->right->val);
-        int target = 0;
-        if(temp.size() == 1)
-            target = -1;
-        int cnt = 0;
-        for(int a:temp)
-            if(cnt == 1)
-                target = a;
+        if(root == nullptr)
+            return -1;
+        if(!(root->left == nullptr && root->right == nullptr)){
+            temp.insert(root->left->val);
+            temp.insert(root->right->val);
+            temp.insert(root->val);
+            long int cnt = 0, memo = 0;
+            if(temp.size() != 1)
+                for(int v:temp){
+                    if(cnt == 1)
+                        memo = v;
+                    cnt++;
+                }
+            if(cnt == 2)
+                ans = min(ans, memo);
+            temp.clear();
+        }
+        findSecondMinimumValue(root->left);
+        findSecondMinimumValue(root->right);
+        return ans != INT64_MAX ? ans : -1;
     }
 };
 // @lc code=end
